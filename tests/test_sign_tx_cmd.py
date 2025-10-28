581a31a98df8f79ff1a0c9a7a102bd5b9bdf2c84
Jun Luo <4catcode@gmail.com>
2025-10-28 11:37:20 +0800

    WIP

--- tests/test_sign_tx_cmd.py
+++ tests/test_sign_tx_cmd.py
@@ -4,6 +4,7 @@ from dataset import SignTxTestCases, MNEMONIC
 import pytest
 from utils import get_testcases_names
 from stellar_sdk import Keypair
+from stellar_sdk.utils import sha256
 from ragger.error import ExceptionRAPDU
 
 
@@ -13,19 +14,19 @@ def test_sign_tx(backend, scenario_navigator, device, navigator, test_name):
     path = "m/44'/148'/0'"
     transaction = getattr(SignTxTestCases, test_name)()
     client = StellarCommandSender(backend)
+    signature_base = transaction.signature_base()
 
     # Send the sign device instruction.
     # As it requires on-screen validation, the function is asynchronous.
     # It will yield the result when the navigation is done
-    with client.sign_tx(path=path, transaction=transaction.signature_base()):
+    with client.sign_tx(path=path, transaction=signature_base):
         # Validate the on-screen request by performing the navigation appropriate for this device
         scenario_navigator.review_approve(
             test_name=f"test_sign_tx_{test_name}", custom_screen_text="Sign "
         )
     response = client.get_async_response().data
 
-    transaction.sign(keypair)
-    expected_signature = transaction.signatures[0].signature
+    expected_signature = keypair.sign(sha256(transaction.signature_base()))
     assert response == expected_signature
 
 
